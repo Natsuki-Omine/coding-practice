a75ba2eda58ba46bf4235b02bee8526d687de47d
Natsuki Omine <166575245+Natsuki-Omine@users.noreply.github.com>
2024-05-18 08:59:58 +0900

    main.jsを更新

--- 中級4/js/main.js
+++ 中級4/js/main.js
@@ -1,39 +1,33 @@
 $(function(){
-    /*=================================================
-    ハンバーガ―メニュー
-    ===================================================*/
+    // ハンバーガ―メニュー
+    
     // ハンバーガーメニューをクリックした時
     $('.header-hamburger').on('click', function() {
-        // ハンバーガーメニューの共通処理を呼び出す
         hamburger();
     });
     // メニューのリンクをクリックした時
     $('.header-navi a').on('click', function() {
-        // ハンバーガーメニューの共通処理を呼び出す
         hamburger();
     });
 
-    /*=================================================
-    Inview（画面に表示されたタイミングで処理を実行）
-    ===================================================*/
+    
+    // Inview（画面に表示されたタイミングで処理を実行）
+    
     // BBBが選ばれる理由（スライド左）
     $('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
         if(isInView){
-        // 要素が表示されたらslide-leftクラスを追加
         $(this).stop().addClass('slide-left');
         }
     });
     // BBBが選ばれる理由（スライド右）
     $('.inview-slide-right').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
         if(isInView){
-        // 要素が表示されたらslide-rightクラスを追加
         $(this).stop().addClass('slide-right');
         }
     });
     // 受講生の声（ふきだし）
     $('.inview-balloon').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
         if(isInView){
-        // 要素が表示されたらballoonクラスを追加
         $(this).stop().addClass('balloon');
         }
     });
