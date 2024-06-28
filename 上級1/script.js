a035efad80a7cc89dd0bf1b9a26ef02db4c6122c
Natsuki Omine <166575245+Natsuki-Omine@users.noreply.github.com>
2024-06-28 14:09:34 +0900

    上級1を更新

--- 上級1/script.js
+++ 上級1/script.js
@@ -1,28 +1,114 @@
 $(function() {
-    // カルーセル
-    $('.pickup_pic').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
+  // カルーセル
+  $('.pickup_pic').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          with: '200px',
+          centerPadding: '50px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // ハンバーガーメニュー
+  document.querySelector('.header-hamburger').addEventListener('click', function() {
+    let header = document.getElementById('header-menu');
+    if (header.classList.contains('open')) {
+      header.classList.remove('open');
+    } else {
+      header.classList.add('open');
+    }
+  });
+
+  // #maskのエリアをクリック時メニューを閉じる
+  document.getElementById('mask').addEventListener('click', function() {
+    document.getElementById('header-menu').classList.remove('open');
+  });
+
+  // リンクのクリック時メニューを閉じる
+  let navLinks = document.querySelectorAll('#header-navi a');
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      document.getElementById('header-menu').classList.remove('open');
     });
+  });
+
+  // スムーススクロール
+  let scrollLinks = document.querySelectorAll('a[hred^="#"]');
+  scrollLinks.forEach(function(scrollLink) {
+    scrollLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      // リンク取得
+      let href = this.getAttribute('href');
+      let target = href === "#" || href === "" ? document.documentElement : document.querySelector(href);
+      // トップからジャンプ先要素までの距離
+      let offsetTop = target.offsetTop;
+
+      scrollTo(document.documentElement, offsetTop, 600);
+    });
+  });
+
+  function scrollTo(element, to, duration) {
+    let begin = element.scrollTop,
+        change = to - begin,
+        currentTime = 0,
+        increment = 20;
+
+    let animateScroll = function() {
+      currentTime += increment;
+      let val = Math.easeInOutQuad(currentTime, begin, change, duration);
+      element.scrollTop = val;
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+    animateScroll();
+  }
+
+  Math.easeInOutQuad = function(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return c /2 * t * t + b;
+    }
+    t--;
+    return -c / 2 * (t * (t - 2) -1) + b;
+  };
+
+  window.addEventListener('scroll', function() {
+    document.querySelectorAll('.fadein').forEach(function(e) {
+      // スクロール距離
+      let scroll = window.scrollY;
+      // fadeinクラス要素までの距離
+      let target = e.offsetTop;
+      // 画面の高さ
+      let windowHeight = window.innerHeight;
+  
+      // fadeinクラス要素が画面下にきてから150px通過したタイミングで表示
+      if (scroll > target - windowHeight + 150) {
+        e.style.opacity = '1';
+        e.style.transform = 'translateY(0)';
+      }
+    });
+
+    this.document.querySelector('.displayOn', function(e) {
+      let scroll2 = window.scrollY;
+      let target2 = e.offsetTop;
+      let windowHeight2 = window.innerHeight;
+      if (scroll2 > target2 - windowHeight2 + 100) {
+        e.style.opacity = '1';
+        e.style.transform = 'tanslateY(0)';
+      }
+    })
+  });
 });
